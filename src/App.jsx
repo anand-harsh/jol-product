b98a64bf261b757e756869cb85c3216bdebe70e8
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2023-09-13 13:38:03 +0530

    Update #1

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import questTheme from 'src/QuestUiKitLightTheme';
+import Products from 'src/components/Products/Products';
+
+function App() {
+  return (
+    <HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={questTheme}>
+          <Routes>
+            <Route path="/" element={<Products />} />
+          </Routes>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </HelmetProvider>
+  );
+}
+
+export default App;
